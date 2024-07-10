d0d042c3a3d0b5e9b33aec9063b6abeda953738f
AluizioCatao <AluizioCatao@DESKTOP-SC84IT8>
2024-07-10 15:17:34 -0500

    Add project files.

---
+++ src/Journey.Exception/ExceptionsBase/JourneyException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Journey.Exception.ExceptionsBase;
+public class JourneyException(string message) : SystemException(message)
+{ 
+}
